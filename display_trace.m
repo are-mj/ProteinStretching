bda3b28c4f9f1157a02972d478d8e1ce092f72c3
are-mj <59837393+are-mj@users.noreply.github.com>
2023-08-30 12:23:59 +0200

    Cleanup. Removed redundant call to anlyse_trace.

--- display_trace.m
+++ display_trace.m
@@ -1,14 +1,16 @@
-function s = display_trace(tbl,xcolumn,ycolumn,fig)
+function slist = display_trace(tbl,xcolumn,ycolumn,fig)
 % Display the traces defined by the data tips in the current figure
 %  tbl: table from analyse_many
 %  xcolumn: Column in tbl on x axis. Default: Deltax
 %  ycolumn: Column in tbl on y axis. Default: Force
+%  fig:     Figure.  Default: current figure.
 %  Example: Create figure by
 %   Tfast = select(Tun,'Pullingspeed>200');
 %   xx = linspace(10,25); figure; 
 %   plot(Tfast.Deltax,Tfast.Force,'.',xx,wlc(xx,.65,290,29.28),'k')
 %  Create data tips
 %  display_trace(Tfast)
+%  or display_trace(Tfast,'Pullingspeed','Deltax',figure(3))
   
   if nargin < 4
     fig = gcf;  % Use current figure if 
@@ -20,15 +22,11 @@ function s = display_trace(tbl,xcolumn,ycolumn,fig)
     xcolumn = 'Deltax';
   end
   [x,y] = read_data_tip(fig);
-  s = [];
+  slist = [];
   for i = 1:length(x)
     T = tbl(tbl.(xcolumn) == x(i) & tbl.(ycolumn) == y(i),:);
-    s = [s;extract_trace(T.Filename,T.Time)];
-    figure;
-    subplot(211);
-    plot(s(i).x,s(i).f);
-    subplot(212);
-    analyse_trace(s(i),1);
+    s = extract_trace(T.Filename,T.Time,1);
+    slist = [slist;s];
   end
 end
 
