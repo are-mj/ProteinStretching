60f84c1cf797fe88cda649a06d075bd5a5f771a9
are-mj <59837393+are-mj@users.noreply.github.com>
2023-10-16 16:19:45 +0200

    Modified COM file name.

--- T_from_COM.m
+++ T_from_COM.m
@@ -1,6 +1,8 @@
 function T = T_from_COM(file)
 % Read temperature from the COM file corresponding to the experiment file
 %   Backup solution if the temperature is not coded in the staus variable
+%   2023-10-16: Modified the construction of COM file name.  Now works for
+%   abA.txt (but no longer for aAB.txt)
   file = char(file);
   slashes = regexp(file,'[\/\\]');
   if numel(slashes)<2  % Short version of filename
@@ -11,7 +13,7 @@ function T = T_from_COM(file)
   end
   strrep(file_full,'\','/');
   
-  fiber = file_full(slashes(end)+1);
+  fiber = file_full(slashes(end)+1:end-5);
   COMfile = [file_full(1:slashes(end)),fiber,'COM.txt'];
   
   fid = fopen(COMfile);
