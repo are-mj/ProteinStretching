aabb7d6184d2f5e5759ae7c62165dce8fab0c702
are-mj <59837393+are-mj@users.noreply.github.com>
2023-10-16 15:40:49 +0200

    Added output: pairs

--- analyse_many.m
+++ analyse_many.m
@@ -1,12 +1,21 @@
-function [Tun,Tre] = analyse_many(files,plotting)
+function [Tun,Tre,pairs] = analyse_many(files,plotting)
 % Analyse a set of experiment data files into tables Tun and Tre
+% pairs: Row numbers in Tre and Tun for event pairs from connected
+%   release-stretch traces
   if nargin < 2
     plotting = 0;
   end
   Tun = [];
   Tre = [];
+  pairs = [];
   for ii = 1:length(files)
-    [Tu,Tr] = analyse_file(files{ii},plotting);
+    [Tu,Tr,~,~,~,peaks] = analyse_file(files(ii),plotting);
+    if nargout > 2
+      pp = findpairs(peaks,Tu,Tr);
+      if ~isempty(pp)
+        pairs = [pairs;pp(:,1:2)+[height(Tre),height(Tun)]];
+      end
+    end
     Tun = [Tun;Tu];
     Tre = [Tre;Tr];
     if plotting
