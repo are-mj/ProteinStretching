6605092c69aea0afccf41006d8d44ac858c139fb
are-mj <59837393+are-mj@users.noreply.github.com>
2023-10-17 13:01:19 +0200

    Problematic files commented out

--- Top7files.m
+++ Top7files.m
@@ -1,6 +1,10 @@
 function files = Top7files
+
+% Combined files: 1 file per experiment
+% folder = 'C:\Users\mjaav_hi6z2cj\OneDrive\Data\Chile\Top7';
+% 126 files in all, 68 deemed good
 files = [    
-    "20220718/bA.txt"
+    % "20220718/bA.txt"    % High dFdx and low Deltax
     "20220718/cAB.txt"
     "20220718/dAE.txt"
     "20220718/eA.txt"
@@ -9,39 +13,78 @@ files = [
     "20230623/bA.txt"
     "20230623/dA.txt"
     "20230623/fA.txt"
+% "20230717/MA.txt" % Events in less than 20% of traces
+% "20230717/aA.txt" % Events in less than 20% of traces
+"20230717/bA.txt"
+"20230717/cA.txt"
+"20230717/cvA.txt"
+% "20230717/dA.txt" % Events in less than 20% of traces
+"20230717/eA.txt"
+% "20230717/fA.txt" % Events in less than 20% of traces
+% "20230717/gA.txt" % Events in less than 20% of traces
+% "20230717/hA.txt" % Events in less than 20% of traces
+% "20230717/iA.txt" % Events in less than 20% of traces
+% "20230717/jA.txt" % Events in less than 20% of traces
+"20230717/kA.txt"
+"20230717/lA.txt"
+"20230717/nA.txt"
+"20230717/oA.txt"
+% "20230720/aA.txt"    % High Deltax at low force
+"20230720/bA.txt"
+% "20230720/cA.txt"    % High dFdx and low Deltax
+"20230720/dA.txt"
+% "20230720/eA.txt"    % High dFdx and low Deltax
+"20230720/fA.txt"
+"20230720/gA.txt"
+"20230720/gaA.txt"
+% "20230720/gbA.txt"  % High dFdx and low Deltax
+% "20230720/gcA.txt"  % High dFdx and low Deltax
+% "20230720/ghA.txt"  % High dFdx and low Deltax
+% "20230720/gnA.txt"
+% "20230720/gsA.txt"  % High dFdx and low Deltax
+% "20230720/gvA.txt"  % High dFdx and low Deltax
+% "20230720/gyA.txt"  % High dFdx and low Deltax
+% "20230720/hA.txt"  % High dFdx and low Deltax
+"20230720/iA.txt"
+"20230720/jA.txt"
+"20230720/kA.txt"
+% "20230720/lA.txt"  % High dFdx and low Deltax
+"20230720/mA.txt"
+"20230720/nA.txt"
+"20230720/oA.txt"    
     "20230721/JA.txt"
     "20230721/JB.txt"
     "20230721/JC.txt"
     "20230721/JD.txt"
-    % "20230721/JE.txt"  % Noisy, unusual.
-    % "20230721/JF.txt"  % Noisy, unusual.
-    "20230721/JG.txt"
-    "20230721/KA.txt"
-    "20230721/KB.txt"
-    "20230721/KC.txt"
+    "20230721/JE.txt"
+    % "20230721/JF.txt"  % High dFdx and low Deltaxclose all
+    % "20230721/JG.txt" % Events in less than 20% of traces
+    % "20230721/KA.txt" % Events in less than 20% of traces
+    % "20230721/KB.txt" % Events in less than 20% of traces
+    % "20230721/KC.txt" % Events in less than 20% of traces
     "20230721/cA.txt"
     "20230721/cB.txt"
-    "20230721/dA.txt"
-    % "20230721/eA.txt"  % No meaningful data
-    "20230721/fA.txt"
-    "20230721/fB.txt"
-    "20230721/fC.txt"
-    % "20230721/fD.txt"  % Noisy, unusual.
+    % "20230721/dA.txt"  % High dFdx and low Deltax
+    % "20230721/eA.txt" % Events in less than 20% of traces
+    % "20230721/fA.txt"  % Very noisy around unfoldings
+    % "20230721/fB.txt"  % High Deltax al low force
+    % "20230721/fC.txt" % Events in less than 20% of traces
+    % "20230721/fD.txt" % Events in less than 20% of traces
     "20230721/fE.txt"
-    "20230721/fF.txt"
+    % "20230721/fF.txt" % Events in less than 20% of traces
     "20230721/fG.txt"
     "20230721/gA.txt"
     "20230721/gB.txt"
-    "20230721/hA.txt"
-    "20230721/iA.txt"
-    "20230722/lA.txt"
-    "20230722/lB.txt"
-    "20230722/lC.txt"
-    "20230722/lD.txt"
-    "20230722/lE.txt"
+    % "20230721/hA.txt"  % High dFdx and low Deltax
+    % "20230721/iA.txt"  % High dFdx and low Deltax
+    % "20230722/lA.txt"  % High dFdx and low Deltax
+    % "20230722/lB.txt"  % High dFdx and low Deltax
+    % "20230722/lC.txt"  % High dFdx and low Deltax
+    % "20230722/lD.txt"  % High dFdx and low Deltax
+    % "20230722/lE.txt"  % High dFdx and low Deltax
     "20230722/mA.txt"
-    "20230722/mB.txt"
-    "20230722/mC.txt"
+    % "20230722/mB.txt"  % High dFdx and low Deltax
+    % "20230722/mC.txt"  % High dFdx and low Deltax
     "20230722/mD.txt"
     "20230722/nA.txt"
     "20230722/nB.txt"
@@ -59,26 +102,34 @@ files = [
     "20230722/pA.txt"
     "20230722/pB.txt"
     "20230722/pC.txt"
-    "20230724/qA.txt"
-    "20230724/rA.txt"
+    % "20230724/qA.txt"  % High dFdx and low Deltax
+    % "20230724/rA.txt"  % High dFdx and low Deltax
     "20230725/SA.txt"
     "20230725/SB.txt"
     "20230725/SC.txt"
     "20230725/SD.txt"
     "20230725/SE.txt"
-    "20230725/SF.txt"
-    "20230725/SG.txt"
-    "20230725/TA.txt"
-    "20230725/TB.txt"
-    "20230725/TC.txt"
-    "20230725/TD.txt"
-    "20230725/TE.txt"
-    "20230725/TF.txt"
+    % "20230725/SF.txt"  % High dFdx and low Deltax
+    % "20230725/SG.txt" % Events in less than 20% of traces
+    % "20230725/TA.txt" % Events in less than 20% of traces
+    % "20230725/TB.txt" % Events in less than 20% of traces
+    % "20230725/TC.txt" % Events in less than 20% of traces
+    % "20230725/TD.txt" % Events in less than 20% of traces
+    % "20230725/TE.txt" % Events in less than 20% of traces
+    % "20230725/TF.txt"  % High dFdx and low Deltax
     "20230725/TG.txt"
-    "20230725/UC.txt"
-    "20230725/UD.txt"
-    "20230725/UE.txt"
-    "20230725/UF.txt"
-    "20230725/UG.txt"
-    "20230725/VA.txt"];
+    % "20230725/UC.txt"  % High dFdx and low Deltax
+    % "20230725/UD.txt"  % High dFdx and low Deltax
+    % "20230725/UE.txt" % Noisy, possible binding problem
+    % "20230725/UF.txt"  % High dFdx and low Deltax
+    % "20230725/UG.txt"  % Noisy, 2 events, possible binding problem
+    % "20230725/VA.txt" % Events in less than 20% of traces
+"20230801/abA.txt"
+"20230801/uA.txt"
+"20230801/vA.txt"
+% "20230801/wA.txt"   % High dFdx and low Deltax
+% "20230801/xA.txt"   % High dFdx and low Deltax
+% "20230801/yA.txt"    % High dFdx and low Deltax
+% "20230801/zA.txt"    % High dFdx and low Deltax
+    ];
 end
