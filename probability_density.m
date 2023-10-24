31d05952fe0d46c9989f9ca2bbe26cb608e02ad9
are-mj <59837393+are-mj@users.noreply.github.com>
2023-10-24 16:47:53 +0200

    Harmonized input and output varible names

--- probability_density.m
+++ probability_density.m
@@ -1,16 +1,16 @@
-function [pd,F,n,Fdot_mean] = probability_density(tbl,dF,plotting)
+function [pd,F,n,Tmean,Fdot] = probability_density(tbl,dF)
 % Histogram of probability densities from experimennts
 % Input:
 %  tbl:  Tu or Tr from unfolding analysis
 %  dF:   Force value spacing
-%  plotting: 0 ( default) NO plotting
-%            1 to display the histogram in new plot
 % Output: 
 %  pd: Column vector of probality desities.  The probability of unfolding 
-%      (or refolding) in the interval <F(i)-dF,F(i+dF)< is pd(i)*dF
+%      (or refolding) in the interval <F(i)-dF/2,F(i+dF/2)< is pd(i)*dF
+%  F: Force vector 
 %  n: nUmber of rows in tbl
-%  F: Force vector (useful if F is not specified in the call)
-%  Fdot_mean: mean value of Fdot (= df/dt, where f(t) is the pulling force)
+%  Tmean: Mean of tbl.Temperature
+%  Fdot: mean value of df/dt before unfolding or refolding, 
+%     where f(t) is the pulling force)
 
 % Version 1.1: Fdot is now negative for refolding traces
 
@@ -27,20 +27,11 @@ function [pd,F,n,Fdot_mean] = probability_density(tbl,dF,plotting)
   if max(tbl.Pullingspeed)/min(tbl.Pullingspeed) > 4
     warning('Events with a very wide range of pullingspeeds should be analysed separately');
   end
-  Fdot_mean = mean(tbl.Fdot);  
+  Fdot = mean(tbl.Fdot);  
+  Tmean = mean(tbl.Temperature);
   n = size(tbl,1);
   edges = (F(1)-0.5*dF):dF:(F(end)+0.5*dF);
   Values = histcounts(tbl.Force,edges);
   pd = Values'/sum(Values)/dF;  % Probability density
-  if plotting
-    figure;
-    histogram(tbl.Force,edges);
-    cla;
-    b = bar(F,pd,1); 
-    set(b,'facecolor',[0.2,.7,.9])
-    title(sprintf('%d events, mean temperaure = %5.1f°C',n,mean(tbl.Temperature)))
-    ylabel('Probability density')
-    xlabel('Force (pN)')
-  end
 end
 
