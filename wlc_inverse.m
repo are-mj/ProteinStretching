b60847bcb3b5086c1c5576342d31936f6c3a728d
are-mj <59837393+are-mj@users.noreply.github.com>
2024-01-25 16:13:57 +0100

    Added input option "Simple": Skip Bouchat correcting polynomial. Modified fzero search interval for better robustness

--- wlc_inverse.m
+++ wlc_inverse.m
@@ -1,16 +1,22 @@
-function x = wlc_inverse(f,P,T,L0)
+function x = wlc_inverse(f,P,T,L0,simple)
 % Worm-like chain extension as function of the force
 % Input:
 %  F: Force (pN)
 %  P: Persistence length (nm)
 %  T: Temperature (K)
 %  L0: Contour length (Molecule length at maximun extension) (nm)
+%  simple: Do not use the improved fit to WLC
 % Output: 
 %  x: Extension of molecule (nm) 
 
+% Version 1.1: Makes sure that 0 < x < L0
+  
+  if nargin < 5
+    simple = 0;
+  end
   x = zeros(size(f));
   for i = 1:length(f)
-    fun = @(x) wlc(x,P,T,L0)-f(i);
-    x(i) = fzero(fun,10);
+    fun = @(x) wlc(x,P,T,L0,simple)-f(i);
+    x(i) = fzero(fun,[0,L0*0.9999]);
   end
 end
