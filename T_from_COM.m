c57b30b34404317eb301e4e4f63f561ae6e52542
are-mj <59837393+are-mj@users.noreply.github.com>
2024-01-25 15:54:04 +0100

    Reads only TemperatureB (as recommended by Steven Smith)

--- T_from_COM.m
+++ T_from_COM.m
@@ -3,6 +3,7 @@ function T = T_from_COM(file)
 %   Backup solution if the temperature is not coded in the staus variable
 %   2023-10-16: Modified the construction of COM file name.  Now works for
 %   abA.txt (but no longer for aAB.txt)
+%   2023-12-07: Read only TemperatureB (as recommended by Steve B. Smith)
   file = char(file);
   slashes = regexp(file,'[\/\\]');
   if numel(slashes)<2  % Short version of filename
@@ -14,6 +15,11 @@ function T = T_from_COM(file)
   strrep(file_full,'\','/');
   
   fiber = file_full(slashes(end)+1:end-5);
+  underscore_pos = regexp(fiber,'_');
+  if ~isempty(underscore_pos)
+    fiber(underscore_pos-1:end) = [];  % handle files like  "20230821/eA_10°.txt"
+  end
+  
   COMfile = [file_full(1:slashes(end)),fiber,'COM.txt'];
   
   fid = fopen(COMfile);
@@ -21,18 +27,15 @@ function T = T_from_COM(file)
   fclose(fid);
   lines = c{1};
   nlines = numel(lines);
-  TA = [];
+  TB = [];
   for j = 1:nlines
-    if contains(lines{j},'temperatureA')
-      equalpos = findstr(lines{j},'=');
-      numbers = sscanf(lines{j}(equalpos(1)+1:end),'%f');
-      TA = [TA;numbers(1)];
-      % data = sscanf(lines{j},'%d %*s %*s %f');
-      % TA = [TA;data(2)];
+    if contains(lines{j},'temperatureB')
+      [~,pos] = regexp(lines{j},'temperatureB =');
+      TB = [TB;str2double(lines{j}(pos+1:numel(lines{j})))];
     end
   end
-  T = round(mean(TA,'omitnan'));
-  if isnan(T)
-    T = 20;   % Just to choose a default
-  end
+  T = round(mean(TB,'omitnan'),2);
+  % if isnan(T)
+  %   T = 20;   % Just to choose a default
+  % end
 end
