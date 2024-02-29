3498766c8ebf5af20d904a85cdd0f950223bf37c
are-mj <59837393+are-mj@users.noreply.github.com>
2024-02-29 16:03:55 +0100

    Clustering paramters may now be an input.  Added plotting option

--- no_outliers.m
+++ no_outliers.m
@@ -1,4 +1,4 @@
-function [ok,Cluster1,Cluster2,par] = no_outliers(Tun)
+function [ok,Cluster1,Cluster2,par] = no_outliers(Tun,par,plotting)
 % Remove outliers as defined by dbscan, keeping only Cluster1 and Cluster2
 % ok:       logical array, true for non-outlier rows in Tun
 % Cluster1: logical array, true for Cluster1 rows in Tun
@@ -6,12 +6,26 @@ function [ok,Cluster1,Cluster2,par] = no_outliers(Tun)
 %
 %  (ok = Cluster | Cluster2,  outliers = ~ok)
 
-  par.epsilon = 5.5;
-  par.minpts = 200;
-  par.scaling = 3;
+  if nargin < 2 || isempty(par)
+    par.epsilon = 5.5;
+    par.minpts = 200;
+    par.scaling = 3;
+  end
+
+
   X = [Tun.Deltax*par.scaling,Tun.Force];  % Scaled
   labels = dbscan(X,par.epsilon,par.minpts);
   Cluster1 = labels == 1;
   Cluster2 = labels == 2;
   ok = labels>0;
+  if nargin>2
+    if plotting
+      figure;
+      plot(Tun.Deltax(Cluster1),Tun.Force(Cluster1),'.b', ...
+        Tun.Deltax(Cluster2),Tun.Force(Cluster2),'.r');
+      hold on;
+      plot(Tun.Deltax(~ok),Tun.Force(~ok),'.','color',0.65*[1 1 1])
+      hold off
+    end
+  end
 end
